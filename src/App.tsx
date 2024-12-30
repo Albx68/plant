26ecf80a218deb3ed8c8592319d296bd811b06a4
Albx68 <albertyumnam3d@gmail.com>
2024-12-30 18:43:43 +0530

    init tailwind

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+
+  return (
+    <>
+      <div className="bg-red-400 text-red-900">
+      <svg width="200" height="100" className="bg-white">
+<circle cx="50" cy="50" r="40" fill="green"/>
+      </svg>
+     </div>
+    </>
+  );
+}
+
+export default App;
