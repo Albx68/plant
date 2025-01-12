221c3bdf072fbc63ce6a20c59b2d02d0f126edb8
Albx68 <albertyumnam3d@gmail.com>
2025-01-12 21:02:50 +0530

    added petal data

---
+++ src/utils/constants/PetalData.ts
@@ -0,0 +1,16 @@
+import { TDataPoint } from "../../components/inputs/SingleSelect";
+import { TPetal } from "../types/Path";
+
+const PetalData: TDataPoint<TPetal>[] = [
+  { name: "ellipse", value: "ellipse" },
+  { name: "tulip", value: "tulip" },
+  { name: "daisy", value: "daisy" },
+  { name: "lily", value: "lily" },
+  { name: "orchid", value: "orchid" },
+  { name: "lotus", value: "lotus" },
+  { name: "cherry", value: "cherryBlossom" },
+  { name: "bluebell", value: "bluebell" },
+  { name: "tear", value: "teardrop" },
+];
+
+export default PetalData;
