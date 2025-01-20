276d38f3db12020edbd10afedadae3aa71be498d
Albx68 <albertyumnam3d@gmail.com>
2025-01-20 11:35:52 +0530

    fix4

--- src/components/screens/Garden.tsx
+++ src/components/screens/Garden.tsx
@@ -1,12 +1,8 @@
-import { flowerData } from "../../App";
-import DefaultFlower from "../flowers/Flower";
 
 const Garden = () => {
   return (
     <div className="flex flex-wrap">
-      {flowerData?.map((flower) => (
-        <DefaultFlower {...flower} />
-      ))}
+
     </div>
   );
 };
